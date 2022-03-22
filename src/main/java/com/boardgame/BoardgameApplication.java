6d5e69ff4d407012be8d1062c284969fb3ebc577
Dave Mayes <davidrmayes@gmail.com>
2022-03-22 14:51:10 -0600

    Updated postgres connection

--- src/main/java/com/boardgame/BoardgameApplication.java
+++ src/main/java/com/boardgame/BoardgameApplication.java
@@ -2,8 +2,9 @@ package com.boardgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class BoardgameApplication {
 
     public static void main(String[] args) {
